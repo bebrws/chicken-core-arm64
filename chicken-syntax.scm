5d90cee98a44637c37b5e691b02d950b05a23240
Brad Barrows <bebrws@gmail.com>
2020-07-17 18:02:44 -0700

    Fixed missing parenz

--- chicken-syntax.scm
+++ chicken-syntax.scm
@@ -97,8 +97,8 @@ EOF
  `((list-ref . scheme#list-ref))
  (##sys#er-transformer
   (lambda (form r c)
-		display 33)
-      (##core#inline "setpixel" 1 2 3 4 5 6 )))
+		(display 33)
+      (##core#inline "setpixel" 1 2 3 4 5 6 ))))
 	;   (##core#inline "setpixel" (car form) (car (cdr form)) (car (cddr form)) (car (cdddr form)) (car (cddddr form)) (car (cdr (cddddr form)))  )))
 
 
